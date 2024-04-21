7493da0ad55a8dc74678ddbaed582cd7489be057
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-04-21 00:16:02 -0500

    Router Initial Support

--- application_layer/routes.js
+++ application_layer/routes.js
@@ -1,7 +1,14 @@
 const http = require("http");
+const express = require('express')
+
+const app = express()
 
 const host = 'localhost';
 const port = 8000;
+
+app.get('/', (req, res) => {
+    response.send('Hello World!')
+})
 
 const requestListener = function (req, res) {};
 
@@ -9,3 +16,4 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+
---
+++ application_layer/test.js
@@ -0,0 +1,32 @@
+const { Client } = require('pg')
+var fs = require('fs')
+var credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'))
+
+const client = new Client(credentials);
+
+client
+    .connect()
+    .then(() => {
+        console.log('Connected to PostgreSQL database');
+
+        client.query('SELECT * FROM libro LIMIT 10', (err,result) => {
+            if(err) {
+                console.error('Error executing query', err);
+            } else {
+                console.log('Query result:', result.rows);
+            }
+
+        client
+            .end()
+            .then(() => {
+                console.log('Connection to PostgreSQL closed');
+            })
+            .catch((err) => {
+                console.error('Error closing connection', err);
+            })
+
+    })
+    })
+    .catch((err) => {
+        console.error('Error connecting to PostgreSQL database', err);
+    });
