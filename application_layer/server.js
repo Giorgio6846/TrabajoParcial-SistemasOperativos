d7920098976a610ac3381dd212840a5c803c5cb7
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-04-25 22:38:08 -0500

    Fixed minor change

--- application_layer/server.js
+++ application_layer/server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const express = require('express')
 const path = require('path')
 const expressHandlebars = require('express-handlebars')
-const { returnBook } = require('./DBconnection')
+//const { returnBook } = require('./DBconnection')
 
 const app = express()
 
