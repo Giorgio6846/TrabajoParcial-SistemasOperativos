67e0912eccfa0c1f0c8830bbec32d62f9e8d870e
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-04-30 19:25:48 -0500

    Search for selected book

--- application_layer/server.js
+++ application_layer/server.js
@@ -9,7 +9,7 @@ const booksTest = JSON.parse(fs.readFileSync('./testBooks.json', 'utf8'))
 
 const router = express()
 
-var production = true
+var production = false
 
 router.use(express.urlencoded({extended: false}))
 
@@ -35,34 +35,18 @@ router.get('/books',async (req,res) =>{
     }
 })
 
-router.get('/book', (req,res) => {
-    if(false) {
-        res.render("home")
-    } else {
-        res.render("book", returnBook(30))
-    }
-})
-
-router.post('/book', async (req,res) =>{
-    if (production) {
-        res.render("book", await returnBook(30))
+router.get('/book', async (req,res) => {
+    if(production) {
+        const idLibro = req.query.idLibro
+    
+        if(!isNaN(idLibro) ) {
+            console.log(idLibro)
+            res.render("book", await returnBook(idLibro))
+        }
+        else {
+            res.render("home")
+        }
     } else {
         res.render("book", bookTest)
     }
-})
-
-router.get('/test/book/json', async (req,res) => {
-    res.send(await returnBook(59))
-})
-
-router.get('/test/book/UI', async (req,res) => {
-    res.render("book", await returnBook(59))
-})
-
-router.get('/test/books/json', async (req,res) => {
-    res.send({booksL: await returnListBooks(20)})
-})
-
-router.get('/test/books/UI', async (req,res) => {
-    res.render("books",{booksL: await returnListBooks(20)})
 })
